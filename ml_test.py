cf238a9bbb7740fe08db923b463332675e7fa405
derpuppo <51084235+derpuppo@users.noreply.github.com>
2025-01-10 14:17:37 +0000

    tmp

--- ml_test.py
+++ ml_test.py
@@ -6,27 +6,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
 
 # Carga de datos
-data = pd.read_csv('stell_metrics_2_train_window2000ms.csv')  # Asume que los datos están exportados como CSV
+data = pd.read_csv('stell_metrics_2_window2000ms.csv')  # Asume que los datos están exportados como CSV
 
 filtered_data = data.loc[(data['epc'] == "EEA12222") & (data['zone'] != "")] # EEA10001, EEA12222, EEA14444
 
 # Características y etiquetas
 # freq_count,rssi_count,rssi_max,rssi_min,rssi_spread,rssi_stdev,zone
-X = filtered_data[['freq_count','rssi_count','rssi_max','rssi_min','rssi_spread','rssi_stdev']]
+X_test = filtered_data[['freq_count','rssi_count','rssi_max','rssi_min','rssi_spread','rssi_stdev']]
 #X = filtered_data[['freq_count','rssi_count','rssi_max']]
-y = filtered_data['zone']  # Etiquetas: 'far', 'med', 'near'
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_test = filtered_data['zone']  # Etiquetas: 'far', 'med', 'near'
 
 scaler = StandardScaler()
 #X_train = scaler.fit_transform(X_train)
 #X_test = scaler.transform(X_test)
 
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf.fit(X_train, y_train)
+clf = joblib.load('rf_model.pkl')
 
 y_pred = clf.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
-
-joblib.dump(clf, 'rf_model.pkl')
